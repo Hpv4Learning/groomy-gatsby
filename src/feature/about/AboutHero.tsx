0881990e4c249a80bcbbc106aa99f12b667a6125
omardeangelis <omar.hpvfilm@gmail.com>
2022-10-04 08:22:09 +0200

    fixato: layout allineato hero about

--- src/feature/about/AboutHero.tsx
+++ src/feature/about/AboutHero.tsx
@@ -13,6 +13,7 @@ const Wrapper = styled("div")<any>(({ theme }: { theme: ThemeType }) => ({
   height: "calc( 100vh - 72px )",
   width: "100%",
   display: "flex",
+  position: "relative",
 }));
 
 const AboutHero = () => {
@@ -21,65 +22,90 @@ const AboutHero = () => {
   return (
     <Wrapper>
       <div
-        className='flex flex-column justify-content-center'
         style={{
-          background: theme.colors.yellow[400],
-          maxWidth: "60%",
-          width: "100%",
-          height: "100%",
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          bottom: "0px",
+          right: "0px",
         }}
       >
         <div
+          className='flex flex-column justify-content-center'
           style={{
-            maxWidth: "768px",
-            width: "calc( 100% - 48px )",
-            margin: "auto",
+            background: theme.colors.yellow[400],
+            maxWidth: "50%",
+            width: "100%",
+            height: "100%",
           }}
-        >
-          <Display weight='semibold'>Chi siamo</Display>
-          <div
-            className='spacer-md'
-            style={{
-              maxWidth: "596px",
-            }}
-          >
-            <Heading>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium rem, totam, voluptatum sed, architecto exercitationem
-              doloremque dolorem optio quae soluta deleniti vitae fugiat dolorum
-              molestias debitis vero commodi officia dolore ad officiis
-              adipisci? Architecto voluptas officia consequuntur a modi totam!
-            </Heading>
-          </div>
-          <div className='spacer-lg'>
-            <Button isRounded color='purple' size='xl'>
-              Contattaci
-            </Button>
-          </div>
-        </div>
+        />
+
+        <div
+          className='flex flex-column justify-content-center'
+          style={{
+            width: "100%",
+          }}
+        />
       </div>
-      <div
-        className='flex flex-column justify-content-center'
+      <Container
         style={{
-          width: "100%",
-          height: "100%",
+          zIndex: 2,
+          display: "flex",
+          justifyContent: "space-between",
         }}
       >
         <div
+          className='flex flex-column justify-content-center'
           style={{
-            margin: "auto",
+            background: theme.colors.yellow[400],
+            width: "100%",
+            height: "100%",
           }}
         >
-          <RoundedImageContainer>
-            <StaticImage
-              src='./images/about.jpeg'
-              alt='una dolce bambina che cucina'
-              placeholder='tracedSVG'
-              loading='eager'
-            />
-          </RoundedImageContainer>
+          <div style={{}}>
+            <Display weight='semibold'>Chi siamo</Display>
+            <div
+              className='spacer-md'
+              style={{
+                maxWidth: "596px",
+              }}
+            >
+              <Heading>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium rem, totam, voluptatum sed, architecto exercitationem
+                doloremque dolorem optio quae soluta deleniti vitae fugiat
+                dolorum molestias debitis vero commodi officia dolore ad
+                officiis adipisci? Architecto voluptas officia consequuntur a
+                modi totam!
+              </Heading>
+            </div>
+            <div className='spacer-lg'>
+              <Button isRounded color='purple' size='xl'>
+                Contattaci
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
+        <div
+          className='flex flex-column justify-content-center'
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "flex-end",
+          }}
+        >
+          <div>
+            <RoundedImageContainer>
+              <StaticImage
+                src='./images/about.jpeg'
+                alt='una dolce bambina che cucina'
+                placeholder='tracedSVG'
+                loading='eager'
+              />
+            </RoundedImageContainer>
+          </div>
+        </div>
+      </Container>
     </Wrapper>
   );
 };
