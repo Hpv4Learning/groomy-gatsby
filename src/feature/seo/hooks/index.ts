768e35469926fa26d30a75bb0c1b813af3ce5a94
omardeangelis <omar.hpvfilm@gmail.com>
2022-11-10 19:47:46 +0100

    add seo starter

---
+++ src/feature/seo/hooks/index.ts
@@ -0,0 +1,6 @@
+export * from "./useSiteMetadata";
+export * from "./useOrganizationSchema";
+export * from "./useWebPageSchema";
+export * from "./useBreadcrumbSchema";
+export * from "./useArticleSchema";
+export * from "./useRecipeSchema";
